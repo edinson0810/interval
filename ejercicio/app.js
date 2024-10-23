4223cc128af03dfeda355a1a5670f7655e6f89a2
edinson0810 <lache081015@gmail.com>
2024-10-22 21:23:15 -0500

    clase 22/10

---
+++ ejercicio/app.js
@@ -0,0 +1,20 @@
+import { getUserById } from "./modulo.js";
+
+ getUserById(8, function (error, user) {
+    if (error){
+
+    }else {
+        console.log(user);
+    }
+    
+});
+
+
+
+
+
+
+
+
+
+
---
+++ ejercicio/data.js
@@ -0,0 +1,78 @@
+export const users = [
+    {
+        id: 1,
+        name: "Jhon Becera",
+        instructor: true
+    },
+    {
+        id: 2,
+        name: "Juan camilo",
+        instructor: false,
+        asignaciones: [2, 3]
+    },
+    {
+        id: 3,
+        name: "valeria",
+        instructor: false,
+        asignaciones: [1, 4]
+    },
+    {
+        id: 3,
+        name: "Hernan",
+        instructor: false,
+        asignaciones: [4, 3]
+    },
+    {
+        id: 4,
+        name: "jaun franco",
+        instructor: false,
+        asignaciones: [1, 2, 3]
+    },
+    {
+        id: 5,
+        name: "camila vesga",
+        instructor: false,
+        asignaciones: [2, 3]
+    },
+    {
+        id: 6,
+        name: "edinson vesga",
+        instructor: false,
+        asignaciones: [4, 3]
+    },
+    {
+        id: 7,
+        name: "daniel",
+        instructor: false,
+        asignaciones: [2, 3]
+    },
+    { 
+        id: 8,
+        name: "fernando",
+        instructor: false,
+        asignaciones: [2, 3]
+    },
+]
+
+export const asignaciones = [
+    {
+        id: 1,
+        name: 'git'
+    },
+    {
+        id: 2,
+        name: 'html'
+    },
+    {
+        id:3,
+        name: 'css'
+    },
+    {
+    id: 3,
+    name: 'Python'
+    },
+    {
+        id: 4,
+        name: 'java'
+    }
+]
