681e6dbe7f32f45c8006de15d4c5ce6476a606ef
edinson0810 <lache081015@gmail.com>
2024-11-01 19:57:26 -0500

    ejercicio 1

--- app.js
+++
@@ -1,8 +0,0 @@
-function callback(message) {
-console.log(message);
-}
-function saludo(name, callback) {
-    let mesagge = `Hola ${name}`
-    callback(mesagge);
-}
-saludo("Edinson Barrios", callback);
