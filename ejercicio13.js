a0049f34fb045c1b5ede63548db863167edf1b78
edinson0810 <lache081015@gmail.com>
2024-11-12 18:45:53 -0500

    ejericico 13

--- ejercicio13.js
+++ ejercicio13.js
@@ -1,27 +1,19 @@
 function miPromesa () {
-    // setTimeout(() => {
+    setTimeout(() => {
         
         return new Promise((resolve => (resolve))  )
-    // }, 3000);
+     }, 3000);
 
 }
 let respuesta =miPromesa()
-console.log((respuesta));
+// console.log((respuesta));
 
-// miPromesa().then(() => console.log("Promesa Resuelta")
-// );
+miPromesa().then(() => console.log("Promesa Resuelta"));
 
-// let numero1 = parseFloat(prompt("ingrese el primer numero"));
-// let numero2 = parseFloat(prompt("ingrese el numero dos"));
-// let suma = numero1 + numero2;
+
 
 // let miPromesa = new Promise(resolve, reject) {
-//    if (suma > 20) {
-//     try {
-//         resolve("Promesa Cumplida" )
-//     } catch (error) {
-//         reject("erro en la promesa")
-//     }}
+//    
 //     setTimeout (()=> {
 //         // resolve("Promesa Resuelta");
 //     }, 3000);
