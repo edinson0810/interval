cb2c31f3863eb7a00186c7cec86f466825b2cb40
edinson barrios <lache081015@gmail.com>
2024-11-18 10:35:06 -0500

    ejercicio 20 finalizado

--- ejercicio20.js
+++ ejercicio20.js
@@ -1,28 +1,29 @@
-function promesa(numero) {
+const promesa1 = function (numero) {
     return new Promise((resolve, reject) => {
-        setInterval(() => {
+        setTimeout(() => {
             resolve (numero)
         }, 1000);
     })
 }
 
-const promesa1 = promesa(10);
-const promesa2 = promesa(15);
-const promesa3 = promesa (20);
-
-promesa1.then((resultado1)=>{
-    console.log("resultado 1:", resultado1 );
-    return promesa2.then((resultado2) =>{
-        console.log("resultado 2:", resultado2);
-        return promesa3.then((resultado3) =>{
-            console.log("rsultado 3:", resultado3);
-            
-        })
+const promesa2 = function (numero) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve (numero)
+        }, 2000);
     })
-})
+}
 
-Promise.all([promesa1, promesa2, promesa3]).then((message) => {
-  console.log("Promesas resueltas", message);
-  
-    });
+const promesa3 = function (numero) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(numero)
+        }, 3000);
+    })
+}
+
+Promise.all([promesa1(10), promesa2(20), promesa3(30)]).then((message) =>{
+    console.log(message);
+    
+})
     
